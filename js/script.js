d2aecc13084f66046df80cc8d78930bfbbc2196b
BigMuscleMann <zanekg123@gmail.com>
2021-10-08 10:26:11 -0500

    a lot

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-let main = {
-    function fetchWeather() =>
+function setBackground(tag, tag2){
+    var height = window.innerHeight
+    var width = window.innerHeight
+    var bgUrl = 'https://source.unsplash.com/' + height + 'x' + width + '/?' + tag + '/' + tag2;
+    document.querySelector('.background').style.backgroundImage = `url(${bgUrl})`
+    console.log(bgUrl)
 }
+
+setBackground('dallas', 'city')
