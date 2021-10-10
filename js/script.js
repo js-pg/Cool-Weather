7db8a67a1300bf77972cb59637cc8523d1d501c6
BigMuscleMann <zanekg123@gmail.com>
2021-10-09 19:26:27 -0500

    main

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
+
 var height = String(window.innerHeight)
 var width = String(window.innerWidth)
 
 function setBackground(tag, tag2){
 
-    var bgUrl = 'https://source.unsplash.com/' + 1920 + 'x' + 1080 + '/?' + tag + '/' + tag2;
-    document.querySelector('.background').style.backgroundImage = `url(${bgUrl})`
+    tag = tag.replace(' ', '+')
+    tag = tag.toLowerCase()
+
+    bgUrl = 'https://source.unsplash.com/1600x900/?' + tag + ',' + tag2;
+    document.querySelector('.background').style.backgroundImage = "url(" + bgUrl + ")"
     console.log(bgUrl)
+    bgUrl = 'https://source.unsplash.com/1600x900/?' + tag + ',' + tag2;
+
 }
 
-setBackground('new-york', 'city')
+
+let main = {
+    "apiKey":"7af0645f81d36888c9fca585b6611eb4",
+    fetchWeather: function (city){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=7af0645f81d36888c9fca585b6611eb4'
+        ).then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+     
+    displayWeather :function (data){
+        const { name } = data;
+        const { icon, description} = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.getElementById('temperature').innerText = temp;
+        document.getElementById('status').innerText = description;
+        document.getElementById('icon').src = "http://openweathermap.org/img/wn/" + icon + ".png"
+        document.getElementById('cityText').innerText = name;
+        setBackground(name, 'city')
+
+    },
+
+
+};
